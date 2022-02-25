e857b6e79f1c9ff1bb470dfc86d6ebb944d12931
KhushbooAit <99725865+KhushbooAit@users.noreply.github.com>
2022-02-25 22:10:59 +0530

    Update main.tf

--- terraform_files/env_resources/main.tf
+++ terraform_files/env_resources/main.tf
@@ -1,7 +1,7 @@
 
 #---------------------------------Key Pair
 resource "aws_key_pair" "auth" {
-  key_name   = join("_", [var.env_name, "webserver_key"])
+  key_name   = ["${join("_", [var.env_name, "webserver_key"])}"]
   public_key = file(var.public_key_path)
 }
 
@@ -9,13 +9,13 @@ resource "aws_key_pair" "auth" {
 #---------------------------------Create IAM Profile
 
 resource "aws_iam_instance_profile" "webserver_profile" {
-  name       = join("_", [var.env_name, "webserver_profile"])
-  role       = aws_iam_role.role.name
-  depends_on = [aws_iam_role.role]
+  name       = ["${join("_", [var.env_name, "webserver_profile"])}"]
+  role       = ["${aws_iam_role.role.name}"]
+  depends_on = ["${aws_iam_role.role}"]
 }
 
 resource "aws_iam_role" "role" {
-  name = join("_", [var.env_name, "webserver_role"])
+  name = ["${join("_", [var.env_name, "webserver_role"])}"]
   path = "/"
 
   assume_role_policy = <<EOF
@@ -36,7 +36,7 @@ EOF
 }
 
 resource "aws_iam_policy" "policy" {
-  name        = join("_", [var.env_name, "webserver_policy"])
+  name        = ["${join("_", [var.env_name, "webserver_policy"])}"]
   description = "web server policy"
 
   policy = <<EOF
@@ -57,28 +57,28 @@ EOF
 }
 
 resource "aws_iam_policy_attachment" "web-attach" {
-  name       = join("_", [var.env_name, "webserver_policy_attachment"])
-  roles      = [aws_iam_role.role.name]
-  policy_arn = aws_iam_policy.policy.arn
+  name       = ["${join("_", [var.env_name, "webserver_policy_attachment"])}"]
+  roles      = ["${aws_iam_role.role.name}"]
+  policy_arn = ["${aws_iam_policy.policy.arn}"]
 }
 
 #---------------------------------Launch Configuration
 
 resource "aws_launch_configuration" "web" {
-  name_prefix     = join("_", [var.env_name, "webserver_"])
-  image_id        = var.ami
-  instance_type   = var.instance_type
-  key_name        = aws_key_pair.auth.id
-  security_groups = [aws_security_group.instance_sg.id]
+  name_prefix     = ["${join("_", [var.env_name, "webserver_"])}"]
+  image_id        = "${var.ami}"
+  instance_type   = "${var.instance_type}"
+  key_name        = ["${aws_key_pair.auth.id}"]
+  security_groups = ["${aws_security_group.instance_sg.id}"]
   user_data = templatefile(var.userdata, {
     env_name = var.env_name
     rds_name = var.rds_name
   })
-  iam_instance_profile = aws_iam_instance_profile.webserver_profile.name
+  iam_instance_profile = ["${aws_iam_instance_profile.webserver_profile.name}"]
   lifecycle {
     create_before_destroy = true
   }
-  depends_on = [aws_key_pair.auth]
+  depends_on = ["${aws_key_pair.auth}"]
 }
 
 
@@ -86,7 +86,7 @@ resource "aws_launch_configuration" "web" {
 #---------------------------------Auto Scaling Group
 
 resource "aws_autoscaling_group" "web" {
-  name                      = join("_", [var.env_name, "webserver"])
+  name                      = ["${join("_", [var.env_name, "webserver"])}"]
   max_size                  = 3
   min_size                  = 1
   health_check_grace_period = 300
@@ -94,12 +94,12 @@ resource "aws_autoscaling_group" "web" {
   health_check_type         = "ELB"
   desired_capacity          = 1
   force_delete              = true
-  launch_configuration      = aws_launch_configuration.web.name
-  vpc_zone_identifier       = var.private_subnets
+  launch_configuration      = "${aws_launch_configuration.web.name}"
+  vpc_zone_identifier       = "${var.private_subnets}"
   depends_on                = [aws_launch_configuration.web, aws_db_instance.application]
   tag {
     key                 = "Name"
-    value               = join("_", [var.env_name, "Web_Server"])
+    value               = ["${join("_", [var.env_name, "Web_Server"])}"]
     propagate_at_launch = true
   }
   lifecycle {
@@ -112,24 +112,24 @@ resource "aws_autoscaling_group" "web" {
 #---------------------------------ALB Target Group
 
 resource "aws_lb_target_group" "web" {
-  name     = join("-", [var.env_name, "target-group"])
+  name     = ["${join("-", [var.env_name, "target-group"])}"]
   port     = 3000
   protocol = "HTTP"
-  vpc_id   = var.vpcid
+  vpc_id   = "${var.vpcid}"
 }
 
 
 #---------------------------------ALB
 resource "aws_lb" "web" {
-  name                             = join("-", [var.env_name, "alb"])
+  name                             = ["${join("-", [var.env_name, "alb"])}"]
   internal                         = false
   load_balancer_type               = "application"
-  security_groups                  = [aws_security_group.alb_sg.id]
-  subnets                          = var.public_subnets
+  security_groups                  = ["${aws_security_group.alb_sg.id}"]
+  subnets                          = "${var.public_subnets}"
   enable_cross_zone_load_balancing = true
 
   tags = {
-    Name = join("_", [var.env_name, "web_alb"])
+    Name = ["${join("_", [var.env_name, "web_alb"])}"]
   }
 }
 
@@ -137,27 +137,27 @@ resource "aws_lb" "web" {
 
 #---------------------------------ALB Listener
 resource "aws_lb_listener" "web_front_end" {
-  load_balancer_arn = aws_lb.web.arn
+  load_balancer_arn = ["${aws_lb.web.arn}"]
   port              = "80"
   protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.web.arn
+    target_group_arn = ["${aws_lb_target_group.web.arn}"]
   }
 }
 
 
 #--------------------------------Target Group Attachment
 resource "aws_autoscaling_attachment" "asg_attachment_web" {
-  autoscaling_group_name = aws_autoscaling_group.web.id
-  alb_target_group_arn   = aws_lb_target_group.web.arn
+  autoscaling_group_name = ["${aws_autoscaling_group.web.id}"]
+  alb_target_group_arn   = ["${aws_lb_target_group.web.arn}"]
 }
 
 
 #--------------------------------Cloud Watch Alarm
 
 resource "aws_cloudwatch_metric_alarm" "high" {
-  alarm_name          = join("_", [var.env_name, "cpu-high-utilization"])
+  alarm_name          = ["${join("_", [var.env_name, "cpu-high-utilization"])}"]
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "2"
   metric_name         = "CPUUtilization"
@@ -167,70 +167,70 @@ resource "aws_cloudwatch_metric_alarm" "high" {
   threshold           = var.highcpu
   alarm_description   = "Scale down web servers when CPU utilization is more than threshold"
   dimensions = {
-    AutoScalingGroupName = aws_autoscaling_group.web.name
+    AutoScalingGroupName = ["${aws_autoscaling_group.web.name}"]
   }
-  alarm_actions = [aws_autoscaling_policy.scaleup.arn]
-  depends_on    = [aws_autoscaling_group.web]
+  alarm_actions = ["${aws_autoscaling_policy.scaleup.arn}"]
+  depends_on    = ["${aws_autoscaling_group.web}"]
 }
 
 resource "aws_cloudwatch_metric_alarm" "low" {
-  alarm_name          = join("_", [var.env_name, "cpu-low-utilization"])
+  alarm_name          = ["${join("_", [var.env_name, "cpu-low-utilization"])}"]
   comparison_operator = "LessThanOrEqualToThreshold"
   evaluation_periods  = "2"
   metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
   period              = "60"
   statistic           = "Average"
-  threshold           = var.lowcpu
+  threshold           = "${var.lowcpu}"
   alarm_description   = "Scale down web servers when CPU utilization is less than threshold"
   dimensions = {
-    AutoScalingGroupName = aws_autoscaling_group.web.name
+    AutoScalingGroupName = ["${aws_autoscaling_group.web.name}"]
   }
-  alarm_actions = [aws_autoscaling_policy.scaledown.arn]
-  depends_on    = [aws_autoscaling_group.web]
+  alarm_actions = ["${aws_autoscaling_policy.scaledown.arn}"]
+  depends_on    = ["${aws_autoscaling_group.web}"]
 }
 
 
 #--------------------------------Auto Scaling Policies
 
 resource "aws_autoscaling_policy" "scaleup" {
-  name                   = join("_", [var.env_name, "add-web-server"])
+  name                   = ["${join("_", [var.env_name, "add-web-server"])}"]
   scaling_adjustment     = 1
   adjustment_type        = "ChangeInCapacity"
   cooldown               = 300
-  autoscaling_group_name = aws_autoscaling_group.web.name
-  depends_on             = [aws_autoscaling_group.web]
+  autoscaling_group_name = ["${aws_autoscaling_group.web.name}"]
+  depends_on             = ["${aws_autoscaling_group.web}"]
 }
 
 resource "aws_autoscaling_policy" "scaledown" {
-  name                   = join("_", [var.env_name, "remove-web-server"])
+  name                   = ["${join("_", [var.env_name, "remove-web-server"])}"]
   scaling_adjustment     = -1
   adjustment_type        = "ChangeInCapacity"
   cooldown               = 300
-  autoscaling_group_name = aws_autoscaling_group.web.name
-  depends_on             = [aws_autoscaling_group.web]
+  autoscaling_group_name = ["${aws_autoscaling_group.web.name}"]
+  depends_on             = ["${aws_autoscaling_group.web}"]
 }
 
 
 #---------------------------RDS Instance
 resource "aws_db_instance" "application" {
-  allocated_storage      = var.db_storage
+  allocated_storage      = "${var.db_storage}"
   storage_type           = "gp2"
   engine                 = "mysql"
   engine_version         = "5.7"
-  instance_class         = var.dbinstance_class
+  instance_class         = "${var.dbinstance_class}"
   name                   = "test"
-  identifier             = join("-", [lower(var.env_name), var.rds_name])
-  username               = var.dbuser
-  password               = var.dbpassword
+  identifier             = ["${join("-", [lower(var.env_name), var.rds_name])}"]
+  username               = "${var.dbuser}"
+  password               = "${var.dbpassword}"
   skip_final_snapshot    = true
   parameter_group_name   = "default.mysql5.7"
-  db_subnet_group_name   = var.dbsubnet
+  db_subnet_group_name   = "${var.dbsubnet}"
   multi_az               = false
   storage_encrypted      = true
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  vpc_security_group_ids = ["${aws_security_group.rds_sg.id}"]
   tags = {
-    Name = join("-", [var.env_name, var.rds_name])
+    Name = ["${join("-", [var.env_name, var.rds_name])}"]
   }
 }
 
@@ -238,14 +238,14 @@ resource "aws_db_instance" "application" {
 #--------------------------------------------------------------------- Create Security Group
 
 resource "aws_security_group" "instance_sg" {
-  name        = join("-", [var.env_name, "instance_sg"])
+  name        = ["${join("-", [var.env_name, "instance_sg"])}"]
   description = "Web Server Security Group"
   vpc_id      = var.vpc_id
   ingress {
     from_port       = 3000
     to_port         = 3000
     protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
+    security_groups = ["${aws_security_group.alb_sg.id}"]
   }
 
   egress {
@@ -262,9 +262,9 @@ resource "aws_security_group" "instance_sg" {
 }
 
 resource "aws_security_group" "alb_sg" {
-  name        = join("-", [var.env_name, "alb_sg"])
+  name        = ["${join("-", [var.env_name, "alb_sg"])}"]
   description = "Application Load Balancer Security Group"
-  vpc_id      = var.vpc_id
+  vpc_id      = "${var.vpc_id}"
   ingress {
     from_port   = 80
     to_port     = 80
@@ -286,14 +286,14 @@ resource "aws_security_group" "alb_sg" {
 
 
 resource "aws_security_group" "rds_sg" {
-  name        = join("-", [var.env_name, "rds_sg"])
+  name        = ["${join("-", [var.env_name, "rds_sg"])}"]
   description = "RDS Instance Security Group"
-  vpc_id      = var.vpc_id
+  vpc_id      = ["${var.vpc_id}"]
   ingress {
     from_port       = 3306
     to_port         = 3306
     protocol        = "tcp"
-    security_groups = [aws_security_group.instance_sg.id]
+    security_groups = ["${aws_security_group.instance_sg.id}"]
   }
   egress {
     from_port   = 0
